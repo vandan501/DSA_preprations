f0b39a45a45c019e9ef8e5605045a8e1b191c91f
Raval Vandan <120940947+vandan501@users.noreply.github.com>
2023-06-25 11:33:29 +0530

    Update moveseros.js

--- mockTEST1/moveseros.js
+++ mockTEST1/moveseros.js
@@ -1,1 +1,22 @@
 
+function moveZeroes(nums) 
+{
+  let currentNumber = 0; 
+  for (let i = 0; i < nums.length; i++) 
+  {
+    if (nums[i] !== 0) {
+      nums[currentNumber] = nums[i];
+      currentNumber++;
+    }
+  }
+
+  while (currentNumber < nums.length) {
+    nums[currentNumber] = 0;
+    currentNumber++;
+  }
+
+  return nums;
+}
+
+console.log(moveZeroes([0, 1, 0, 3, 12])); 
+console.log(moveZeroes([0])); 
